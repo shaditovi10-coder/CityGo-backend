9f0994840c2fbf16f8bc87276439a6c4c90d5715
Shadi <shadi.tovi10@gmail.com>
2026-05-07 05:17:37 +0300

    Change to MONGODB_URL for Railway

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
@@ -19,5 +19,5 @@ app.get('/', (req, res) => {
   res.send('CityGo API is running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
